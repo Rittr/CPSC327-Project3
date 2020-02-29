de55543e4fec69f7c9b97f31c05a1084f5b0bc86
Chris Ritter <chrisritter2@gmail.com>
2020-02-29 02:00:14 -0500

    backup push

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,4 +1,6 @@
 #include "../includes_usr/fileIO.h"
+#include <fstream>
+#include <sstream>
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
@@ -7,7 +9,36 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
-	return SUCCESS;
+	books.clear();
+
+	ifstream fileToBooks;
+	fileToBooks.open(filename, ios::in);
+	if (fileToBooks.is_open()) {
+		int bookID;
+		string title;
+		string author;
+		book_checkout_state state;
+		int loanerID;
+
+		//int numLine = 0;
+		string line;
+		while (!fileToBooks.eof()) {
+			getline(fileToBooks, line);
+			istringstream ss(line);
+			ss >> bookID >> title >> author >> state >> loanerID;
+			//books[numLine] << line;
+			books.emplace_back(bookID, title, author, state, loanerID);
+
+
+			//numLine++;
+			//I'll probably have to be more specific about where this line is going
+		}
+		if (books.size() < 1) {
+			return NO_BOOKS_IN_LIBRARY;
+		}
+		return SUCCESS;
+	}
+		return COULD_NOT_OPEN_FILE;
 }
 
 /* serializes books to the file filename
@@ -27,7 +58,24 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
-	return SUCCESS;
+	patrons.clear();
+
+	ifstream fileToPatrons;
+	fileToPatrons.open(filename, ios::in);
+	if (fileToPatrons.is_open()) {
+		string line;
+		while (!fileToPatrons.eof()) {
+			getline(fileToPatrons, line);
+			//patrons << line;
+			//I'll probably have to be more specific about where this line is going
+		}
+		if (patrons.size() < 1) {
+			return NO_PATRONS_IN_LIBRARY;
+		}
+		return SUCCESS;
+	}
+		return COULD_NOT_OPEN_FILE;
+
 }
 
 /* serializes patrons to the file filename
